7853e9e5ff298484b3cd2e04610b543695d391aa
Michael Bestas <mkbestas@lineageos.org>
2018-01-27 06:39:34 +0200

    chiron: Add stock build fingerprint
    Change-Id: I508803723c9dce25ebdaaaa5d22abf17fa4503fd

--- lineage.mk
+++ lineage.mk
@@ -10,3 +10,7 @@ PRODUCT_MODEL := Mi MIX 2
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/chiron/chiron:7.1.1/NMF26X/V9.2.3.0.NDEMIEK:user/release-keys \
+    PRIVATE_BUILD_DESC="chiron-user 7.1.1 NMF26X V9.2.3.0.NDEMIEK release-keys"
