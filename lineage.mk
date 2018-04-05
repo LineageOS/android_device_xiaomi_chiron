9a028c687deb4749a4dcbdb29159920b0631d135
Michael Bestas <mkbestas@lineageos.org>
2018-04-05 15:32:09 +0300

    chiron: Update fingerprint to latest global stable
    Change-Id: If9d38d95dca864e14568cedd155364a592bb9f19

--- lineage.mk
+++ lineage.mk
@@ -12,5 +12,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/chiron/chiron:7.1.1/NMF26X/V9.2.3.0.NDEMIEK:user/release-keys \
-    PRIVATE_BUILD_DESC="chiron-user 7.1.1 NMF26X V9.2.3.0.NDEMIEK release-keys"
+    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
