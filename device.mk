578418fff008f6f7dc7dd0846808145e639b0d92
Michael Bestas <mkbestas@lineageos.org>
2018-07-13 20:16:09 +0300

    chiron: Add dummy fingerprint keylayouts
    * Required to filter out home key events from fingerprint scanner
    
    Change-Id: I2575ca7be327eac5f68d9e89cf17d610bc9aa13f

--- device.mk
+++ device.mk
@@ -29,7 +29,9 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx.kl
+    $(LOCAL_PATH)/configs/keylayout/synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
 # Properties
 include $(LOCAL_PATH)/vendor_prop.mk
