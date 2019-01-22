721af00debe91258cc359a8a400572b74ec9678a
Vol Zhdanov <wight554@gmail.com>
2019-01-22 22:59:13 +0000

    chiron: disable default color mode
    Change-Id: I0171971301ee00664f9a6c11fa4851763b9ba9fb

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,3 +2,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qti.sensors.noneui=true \
     ro.xiaomi.sar_fusion=true
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.enable_default_color_mode=0
