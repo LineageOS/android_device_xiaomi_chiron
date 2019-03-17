39f70838b8530005a018ef2f8b5d50e5ebfd0671
Michael Bestas <mkbestas@lineageos.org>
2019-03-17 17:57:07 +0200

    chiron: Update build fingerprint from V10.2.2.0
    Change-Id: I2042d08478b7ffb6aa46c6ee83e8827fbb067dcc

--- lineage_chiron.mk
+++ lineage_chiron.mk
@@ -33,6 +33,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
+    PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V10.2.2.0.ODEMIXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V10.2.2.0.ODEMIXM:user/release-keys
