b31bcf82aae642f438624379747fc11ac22462d1
Arian <arian.kulmer@web.de>
2019-12-02 20:59:15 +0100

    chiron: Remove android.hidl.base@1.0 dependency on fingerprint blobs
    Change-Id: Ib3a2bf6a44f6113e449cd10059c6055c22e4d71c

--- extract-files.sh
+++ extract-files.sh
@@ -17,12 +17,18 @@
 
 function blob_fixup() {
     case "${1}" in
+    vendor/lib64/com.fingerprints.extension@1.0.so)
+        patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
+        ;;
     vendor/lib64/libultrasound.so)
         patchelf --remove-needed "libmedia.so" "${2}"
         ;;
     vendor/lib64/sensors.elliptic.so)
         sed -i "s|/etc/elliptic_sensor.xml|/vendor/etc/elliptic.xml|g" "${2}"
         patchelf --remove-needed "libandroid.so" "${2}"
+        ;;
+    vendor/lib64/vendor.goodix.hardware.fingerprintextension@1.0.so)
+        patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
         ;;
     esac
 }
